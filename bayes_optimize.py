eefa6b634bb271e57213de24bee60aa3b0062e93
Nhi Huỳnh <yennhi24768@gmail.com>
2025-07-20 02:20:13 +0700

    Add Bayesian Optimization

---
+++ bayes_optimize.py
@@ -0,0 +1,45 @@
+import csv
+from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
+from chemprop.parsing import parse_train_args, modify_train_args
+from train import cross_validate
+
+search_space = {
+    'dropout': hp.uniform('dropout', 0.0, 0.5), # tỉ lệ bỏ học
+    'depth': hp.choice('depth', [3, 4, 5]), # số bước truyền tin
+    'hidden_size': hp.choice('hidden_size', [300, 350, 400, 450, 500, 550, 600]), # số lượng nơ-ron trong lớp ẩn
+    'ffn_num_layers': hp.choice('ffn_num_layers', [2, 3, 4, 5]) # số lớp ẩn của FFN
+}
+
+trials = Trials()
+
+
+def objective(params):
+    args = parse_train_args()
+    args.no_cuda = True # GPU => đổi thành False
+    args.dataset_type = 'classification'
+    args.metric = 'auc'
+    args.data_path = './data/bbbp.csv'
+    args.dropout = params['dropout']
+    args.depth = params['depth']
+    args.hidden_size = params['hidden_size']
+    args.ffn_num_layers = params['ffn_num_layers']
+    modify_train_args(args)
+    auc, _ = cross_validate(args)
+    return {'loss': -auc, 'status': STATUS_OK, 'params': params, 'auc': auc}
+
+
+best = fmin(
+    fn=objective, 
+    space=search_space, 
+    algo=tpe.suggest,
+    max_evals=120, # khảo sát 120 bộ tham số (120 model)
+    trials=trials)
+
+with open('bayes_optimize_results.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['dropout', 'depth', 'hidden_size', 'ffn_num_layers', 'test_AUC'])
+    for trial in trials.trials:
+        p = trial['result']['params']
+        writer.writerow([p['dropout'], p['depth'], p['hidden_size'], p['ffn_num_layers'], trial['result']['auc']])
+
+print('Best hyperparameters:', best)
